0e3b40bd26067b631b405a72abf64408bf2bea0e
ChrHac <66358523+ChrHac@users.noreply.github.com>
2020-07-02 15:46:54 +0100

    Search Bar Styling
    JS and CSS added to search bar element

--- js.js
+++ js.js
@@ -1,11 +1,25 @@
 $(document).ready(function(){
-    $("#apps").click(function(){
-      $("#main").toggle();
-    });
+  $("#apps").click(function(){
+    $("#main").toggle();
   });
+});
 
-  $(document).on('click', function (e) {
-    if ($(e.target).closest("#apps").length === 0) {
-        $("#main").hide();
-    }
+$(document).on('click', function (e) {
+  if ($(e.target).closest("#apps").length === 0) {
+      $("#main").hide();
+  };
+});
+
+$(document).ready(function(){
+  $("input").click(function(){
+    document.getElementById("search-bar").style.height = "400px";
+    document.getElementById("buttons").style.marginTop ="-75px";
+  });
+});
+
+$(document).on('click', function (e) {
+  if ($(e.target).closest("#input").length === 0) {
+    document.getElementById("search-bar").style.height = "45px";
+    document.getElementById("buttons").style.marginTop ="11px";
+  };
 });
