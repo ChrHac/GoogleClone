87d148a812236519aa1118afacda4b573797996c
ChrHac <66358523+ChrHac@users.noreply.github.com>
2020-07-02 17:45:14 +0100

    Adding ability to remove search suggestions
    Also adjusted search icon and border radius when extending search bar

--- js.js
+++ js.js
@@ -2,6 +2,12 @@ $(document).ready(function(){
   $("#apps").click(function(){
     $("#main").toggle();
   });
+
+  $("input").click(function(){
+    document.getElementById("search-bar").style.height = "400px";
+    document.getElementById("search-bar").style.borderRadius = "22px";
+    document.getElementById("buttons").style.marginTop ="-75px";
+  });
 });
 
 $(document).on('click', function (e) {
@@ -10,16 +16,14 @@ $(document).on('click', function (e) {
   };
 });
 
-$(document).ready(function(){
-  $("input").click(function(){
-    document.getElementById("search-bar").style.height = "400px";
-    document.getElementById("buttons").style.marginTop ="-75px";
-  });
-});
-
 $(document).on('click', function (e) {
-  if ($(e.target).closest("#input").length === 0) {
+  if ($(e.target).closest("#search-bar").length === 0) {
     document.getElementById("search-bar").style.height = "45px";
+    document.getElementById("search-bar").style.borderRadius = "30px";
     document.getElementById("buttons").style.marginTop ="11px";
   };
+});
+
+$(".remove").click((e) => {
+  $(e.target).parents(".option").hide();
 });
